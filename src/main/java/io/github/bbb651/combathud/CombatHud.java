1aea59482e038b0a38ce4cb6851e313a4d50ec6b
bbb651 <53972231+bbb651@users.noreply.github.com>
2020-07-31 00:14:46 +0300

    Initial commit

--- src/main/java/io/github/bbb651/combathud/CombatHud.java
+++ src/main/java/io/github/bbb651/combathud/CombatHud.java
@@ -1,8 +1,8 @@
-package net.fabricmc.example;
+package io.github.bbb651.combathud;
 
 import net.fabricmc.api.ModInitializer;
 
-public class ExampleMod implements ModInitializer {
+public class CombatHud implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
@@ -11,4 +11,5 @@ public class ExampleMod implements ModInitializer {
 
 		System.out.println("Hello Fabric world!");
 	}
+
 }
